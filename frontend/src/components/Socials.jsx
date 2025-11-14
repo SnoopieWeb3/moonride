289ce87bdd452240ef7b9d04cee6d158d2f0be24
Edinyanga <elzucky@gmail.com>
2025-11-14 13:16:20 +0100

    remove unnecessary socials

--- frontend/src/components/Socials.jsx
+++ frontend/src/components/Socials.jsx
@@ -5,7 +5,7 @@ const Socials = () => {
         <div className='socials py-3'>
             <img src={getImageUrl('telegram.png')} className='social mx-2' onClick={()=>open('https://t.me/moonridedotfun')} />
             <img src={getImageUrl('discord.png')} className='social mx-2' onClick={()=>open('https://discord.com/invite/EjR6CzZ9Su')}/>
-            <img src={getImageUrl('x.png')} className='social mx-2 me-0' onClick={()=>open('https://x.com/moonridedotfun')} />
+            {/*<img src={getImageUrl('x.png')} className='social mx-2 me-0' onClick={()=>open('https://x.com/moonridedotfun')} />*/}
         </div>
     );
 }
