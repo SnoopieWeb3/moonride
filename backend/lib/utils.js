d82fbe8f012839252c5989c04b87d56f4b0a1789
Edinyanga <elzucky@gmail.com>
2025-11-15 06:24:07 +0100

    use proper testnet wss

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -10,7 +10,7 @@ const randomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const bscTestnetWss = 'wss://bsc-rpc.publicnode.com';
+const bscTestnetWss = 'wss://bsc-testnet.drpc.org';
 
 const getPublicClient = async () => {
 
